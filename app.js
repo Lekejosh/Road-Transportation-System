8a59abef82b0b3b7e21e5b8f23880f6195e86fdb
lekejosh <lekejosh6wf@gmail.com>
2023-03-14 23:37:55 +0100

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    httpOnly: true,
+    httpOnly: false,
     expires: Date.now() + 1000 + 60 * 60 * 24 * 7,
     maxAge: 1000 + 60 * 60 * 24 * 7,
   },
