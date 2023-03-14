b2f84e8fe93fdb4eef53d08479dd56836f06bf79
lekejosh <lekejosh6wf@gmail.com>
2023-03-14 23:36:30 +0100

    cookie

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middlewares/error");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const checkTrips = require("./middlewares/serviceWorker");
-const cors = require("cors");
-const { Cookies } = require("universal-cookie-express");
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors())
 
 setInterval(checkTrips, 60 * 1000);
 
-const cookies = new Cookies();
 const sessionConfig = {
   secret: "egeGBTCTEcgwrtgc54cg66666666h.b/3/3.b/[g[er2",
   resave: false,
@@ -24,7 +25,6 @@ const sessionConfig = {
     maxAge: 1000 + 60 * 60 * 24 * 7,
   },
 };
-app.use(cookies.express());
 app.use(session(sessionConfig));
 
 const user = require("./routes/userRoute");
@@ -34,14 +34,9 @@ const order = require("./routes/orderRoute");
 const admin = require("./routes/adminRoute");
 
 // Routes
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      message: "Welcome to Road Transport Resrvation API",
-    });
-});
+app.get("/",(req,res)=>{
+  res.status(200).json({success:true,message:"Welcome to Road Transport Resrvation API"})
+})
 app.use("/api/v1/user", user);
 app.use("/api/v1/driver", driver);
 app.use("/api/v1/transport", transport);
