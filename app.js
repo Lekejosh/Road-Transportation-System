4099166169a4ad8f8e1bb56fb03a50d20e3e8acc
lekejosh <lekejosh6wf@gmail.com>
2023-03-18 03:37:52 +0100

    CORS

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(
+  cors({
+    origin: [process.env.CORS],
+  })
+);
 
 setInterval(checkTrips, 60 * 1000);
 
