91ec186c3958af158fff54093b5757d880930323
lekejosh <lekejosh6wf@gmail.com>
2023-03-20 23:38:50 +0100

    allowedOrigin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,3 +1,7 @@
-const allowedOrigin = [process.env.CORS_1, process.env.CORS_2];
+const allowedOrigin = [
+  process.env.CORS_1,
+  process.env.CORS_2,
+  process.env.CORS_3,
+];
 
-module.exports = allowedOrigin
+module.exports = allowedOrigin;
