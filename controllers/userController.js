edb11ffeae056d556c9842b3d64d88a801cbc75c
lekejosh <lekejosh6wf@gmail.com>
2023-03-28 14:35:58 +0100

    reset Passsword

--- controllers/userController.js
+++ controllers/userController.js
@@ -296,7 +296,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   const resetPasswordUrl = `${req.protocol}://${req.get(
     "origin"
-  )}/api/v1/user/password/reset/${resetToken}`;
+  )}/password/reset/${resetToken}`;
 
   const message = `Your password reset Token is :-\n\n ${resetPasswordUrl} \n\nif you have not requested this email then, please Ignore it`;
   try {
