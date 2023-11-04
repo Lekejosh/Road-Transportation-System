3fef118876b8966e03136c1cbdd365810e243d0f
lekejosh <lekejosh6wf@gmail.com>
2023-11-04 18:49:50 +0100

    feat: block ride creation that is more than 7 days from current date

--- src/services/transport.service.ts
+++ src/services/transport.service.ts
@@ -1,7 +1,6 @@
 import Transport from "../models/transport.model";
 import User from "../models/user.model";
 import CustomError from "../utils/custom-error";
-import moment from "moment";
 
 class TransportService {
     async create(data: TransportCreateInput, userId: string) {
@@ -9,6 +8,8 @@ class TransportService {
         if (!driver) throw new CustomError("Driver not found", 404);
 
         data.driverId = userId;
+
+        if (!(await this.checkIfDateIsSevenDaysFromPresentDay(data.departureDate))) throw new CustomError("You can't create that is more than 7 days from now");
 
         const amountOfTrip = (await this.calculateLast5DepartureDays(userId, data.departureDate)).length;
         if (amountOfTrip >= 4) {
@@ -19,6 +20,16 @@ class TransportService {
 
         const trip = await Transport.create(data);
         return trip;
+    }
+    async checkIfDateIsSevenDaysFromPresentDay(departureDate: string) {
+        const currentDate = new Date();
+        const endDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+        const parsedDepartureDate = new Date(departureDate);
+
+        const isWithinSevenDays = parsedDepartureDate >= currentDate && parsedDepartureDate <= endDate;
+
+        return isWithinSevenDays;
     }
 
     async calculateLast5DepartureDays(driverId: string, date: string) {
@@ -39,51 +50,8 @@ class TransportService {
     }
     async checkifAnotherzTripAlreadyCreatedOnThatDateByTheSameDriver(date: string, driverId: string) {
         const trip = await Transport.findOne({ driverId: driverId, departureDate: date });
-
         return trip;
     }
-    async calculateDepartureTime(tripId: string, driverId: string) {
-        const trip = await Transport.findOne({ _id: tripId, driverId: driverId });
-        if (!trip) throw new CustomError("Trip not found", 404);
-
-        const mainTripDate = trip.departureDate;
-        const mainTripStartDate = new Date(mainTripDate);
-        const mainTripEndDate = new Date(mainTripStartDate);
-        mainTripEndDate.setDate(mainTripEndDate.getDate() + 1);
-
-        console.log(mainTripStartDate);
-
-        const eightAM = new Date(mainTripStartDate);
-        eightAM.setHours(8, 0, 0, 0);
-
-        const eightPM = new Date(mainTripStartDate);
-        eightPM.setHours(20, 0, 0, 0);
-
-        const availableTrips = await Transport.find({
-            origin: trip.origin,
-            destination: trip.destination,
-            departureDate: mainTripStartDate,
-            type: trip.type,
-            _id: { $ne: tripId }
-        });
-
-        for (const availableTrip of availableTrips) {
-            const departureTime = availableTrip.departureTime;
-
-            if (departureTime >= eightAM && departureTime <= eightPM) {
-                const newDepartureTime = new Date(departureTime);
-                newDepartureTime.setMinutes(departureTime.getMinutes() + 30);
-                trip.departureTime = newDepartureTime;
-            } else {
-                const nextDay = new Date(mainTripStartDate);
-                nextDay.setDate(nextDay.getDate() + 1);
-                trip.departureDate = nextDay;
-                trip.departureTime.setHours(8, 0, 0, 0);
-            }
-            await trip.save();
-        }
-
-        console.log(availableTrips);
-    }
+    async calculateDepartureTime(tripId: string, driverId: string) {}
 }
 export default new TransportService();
